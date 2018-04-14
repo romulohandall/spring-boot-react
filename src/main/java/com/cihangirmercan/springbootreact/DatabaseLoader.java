6a8a32cd785638cf005365c563dd6647a2df614b
Cihangir Mercan <mercancihangir@gmail.com>
2018-04-14 03:59:16 +0300

    First Commit

---
+++ src/main/java/com/cihangirmercan/springbootreact/DatabaseLoader.java
@@ -0,0 +1,24 @@
+package com.cihangirmercan.springbootreact;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+	private final TodoRepository repository;
+
+	@Autowired
+	public DatabaseLoader(TodoRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.repository.save(new Todo("Learn Spring Boot", "27.05.2018", "No"));
+		this.repository.save(new Todo("Learn React", "27.05.2018", "No"));
+		this.repository.save(new Todo("Gönüle hasret yazıldı, sevgiye mezar kazıldı", "14.04.2018", "Yes"));
+
+	}
+}
