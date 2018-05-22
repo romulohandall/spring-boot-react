11a18e1c0c31ddc79f079fe618c749a49f662808
Cihangir Mercan <mercancihangir@gmail.com>
2018-05-22 10:18:35 +0300

    Delete TodoRepository.java

--- src/main/java/com/cihangirmercan/springbootreact/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cihangirmercan.springbootreact;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TodoRepository extends PagingAndSortingRepository<Todo, Long> {
-
-}
